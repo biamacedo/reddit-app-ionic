6a5429760b060189c5addfd9e7a8133431fb55f2
biamacedo <bia.macedo13@gmail.com>
2016-01-02 14:32:11 -0200

    Added pull to refresh and infinit scrolling

--- www/js/app.js
+++ www/js/app.js
@@ -5,16 +5,39 @@ app.controller('RedditCtrl', function($http, $scope){
 
     $scope.stories = [];
 
-    $http.get('http://www.reddit.com/r/Android/new/.json')
-         .success(function(response){
-             angular.forEach(response.data.children, function(child){
-                 $scope.stories.push(child.data);
-             });
-         })
-         .error(function(error){
+    function loadStories(params, callback){
+        $http.get('http://www.reddit.com/r/android/new/.json', {params: params})
+        .success(function(response){
+            var stories = [];
+            angular.forEach(response.data.children, function(child){
+                var story = child.data;
+                if (!story.thumbnail || story.thumbnail === 'self' || story.thumbnail === 'default'){
+                    story.thumbnail = 'http://www.redditstatic.com/icon.png';
+                }
+                stories.push(story);
+            });
+            callback(stories);
+        });
+    };
 
-         })
+    $scope.loadOlderStories = function(){
+        var params = {};
+        if ($scope.stories.length > 0){
+            params['after'] = $scope.stories[$scope.stories.length - 1].name;
+        }
+        loadStories(params, function(olderStories){
+            $scope.stories = $scope.stories.concat(olderStories);
+            $scope.$broadcast('scroll.infiniteScrollComplete');
+        });
+    };
 
+    $scope.loadNewerStories = function(){
+        var params = {'before': $scope.stories[0].name};
+        loadStories(params, function(newerStories){
+            $scope.stories = newerStories.concat($scope.stories);
+            $scope.$broadcast('scroll.refreshComplete');
+        });
+    };
 });
 
 app.run(function($ionicPlatform) {
